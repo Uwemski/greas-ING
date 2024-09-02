69d9539e29ff51329cab8292b42a4a869fed0091
Uffort Uwem <iconuwemfrank@gmail.com>
2024-09-02 16:23:36 +0100

    just entered the js file. let's do this lads!

---
+++ script.js
@@ -0,0 +1,6 @@
+/*to save ourself the stress of typing long and keeping it short*/
+var pop = document.getElementById("popup");
+var popUpContent = document.getElementById("popUpContent");
+
+//create a function for your pop up dialogue box
+
