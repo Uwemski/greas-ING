dba2e6b841300251065c9417219f0860936b6143
Uffort Uwem <iconuwemfrank@gmail.com>
2024-09-02 17:28:52 +0100

    added the dialogue with the onclick event listener. Now what if we want to remove it ?

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 /*to save ourself the stress of typing long and keeping it short*/
-var pop = document.getElementById("popup");
-var popUpContent = document.getElementById("popUpContent");
+var pop, link;
 
+pop = document.getElementById("popUp");
+link = document.getElementById("popupContent");
 //create a function for your pop up dialogue box
 
+link.onclick = function (event) {
+    event.preventDefault();
+    pop.style.display = "flex";
+}
