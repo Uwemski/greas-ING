c837b0089f2f7deaf473dae6e65c76133c9933e4
Uffort Uwem <iconuwemfrank@gmail.com>
2024-09-05 13:25:05 +0100

    added a functionality for our unicode by manipulating the beautiful DOM of course.

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 /*to save ourself the stress of typing long and keeping it short*/
-var pop, link;
+var pop, link, dismiss;
 
 pop = document.getElementById("popUp");
 link = document.getElementById("popupContent");
+dismiss = document.getElementById("close-btn");
+
 //create a function for your pop up dialogue box
 
 link.onclick = function (event) {
     event.preventDefault();
-    pop.style.display = "flex";
+    pop.style.display = "flex"; 
 }
+
+dismiss.onclick = function () {
+    pop.style.display = "none"; // Hides the pop-up
+}
+
